96594cfe2dec27bdd342b4d3ee5bc4657215d0c9
Devang Saraogi <devangsaraogi@outlook.com>
2023-10-03 01:47:50 +0530

    Time: 18 ms (52.25%), Space: 19.9 MB (8.67%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int d=0;
-        int i=0;
-        while(d<nums.size()) {
-            if(nums[i]==0) {
-                nums.push_back(0);
-                nums.erase(nums.begin()+i);
-                d++;
-            }
-            else {
-                d++; i++;
-            }
+        int j=0;
+        for(int i=0; i<nums.size(); ++i) {
+            if(nums[i]!=0) nums[j++]=nums[i];
         }
+
+        for(; j<nums.size(); ++j) nums[j]=0;
     }
+
+    // void moveZeroes(vector<int>& nums) {
+    //     int d=0;
+    //     int i=0;
+    //     while(d<nums.size()) {
+    //         if(nums[i]==0) {
+    //             nums.push_back(0);
+    //             nums.erase(nums.begin()+i);
+    //             d++;
+    //         }
+    //         else {
+    //             d++; i++;
+    //         }
+    //     }
+    // }
 };
