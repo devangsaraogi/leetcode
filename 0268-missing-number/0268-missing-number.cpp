172aaf9071a7e95abb0afcffbc495b2b99353ecd
Devang Saraogi <devangsaraogi@outlook.com>
2023-10-05 15:59:01 +0530

    Time: 20 ms (20.63%), Space: 18.2 MB (25.48%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
+    // simple approach
     int missingNumber(vector<int>& nums) {
-        //simple approach
-        int n = nums.size();
-        int check_sum = (n * (n+1)) / 2;
+        int check_sum = (nums.size() * (nums.size()+1)) / 2;
+        
         int sum = 0;
-
         for(auto ele: nums) sum += ele;
-
         return check_sum - sum;
     }
 };
