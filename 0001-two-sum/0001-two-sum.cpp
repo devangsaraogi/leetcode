48c234691194191e67f50eb221bbe698c8be8417
Devang Saraogi <devangsaraogi@outlook.com>
2025-02-11 13:07:55 -0500

    Time: 118 ms (8.39%), Space: 14.1 MB (76.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -18,3 +18,22 @@ public:
             return result;
     }
 };
+
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> result;
+
+    for(int i=0; i<nums.size(); i++){
+        for(int j=0; j<nums.size(); j++){
+            if (nums[i]+nums[j]==target && i!=j){
+                result.push_back(i);
+                result.push_back(j);
+
+                goto jump;
+            }
+        }
+    }
+
+    jump:
+        return result;
+}
