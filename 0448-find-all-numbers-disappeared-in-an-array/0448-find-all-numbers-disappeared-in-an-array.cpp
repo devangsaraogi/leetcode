36d7bda17f6c6ec8ab32e52cd14ce179af107a2f
Devang Saraogi <devangsaraogi@outlook.com>
2025-02-20 18:52:23 -0500

    Time: 4 ms (68.91%), Space: 53.3 MB (48.43%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        // declare map
-        // add elements of vector to map as key and put value 1
-        // iterate through map to push out values which don't have anything assigned
-
+        vector<bool> track(nums.size()+1);
         vector<int> result;
-        map<int, int> track;
 
-        for(int i=1; i<=nums.size(); i++) track[i] = 0;
-        for(auto num:nums) {
-            track[num] = 1;
-        }
-
-        for(auto key:track) {
-            if(key.second == 0) result.push_back(key.first);
+        for(auto num:nums) track[num] = true;
+        for(int i=1; i<=nums.size(); i++) {
+            if(!track[i]) result.push_back(i);
         }
 
         return result;
