b61442e3ae61b43f416f1319191fdb35b18b60ba
Devang Saraogi <devangsaraogi@outlook.com>
2026-01-29 17:04:33 -0500

    Time: 0 ms (100%), Space: 14.9 MB (36.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> um;
-        for(int i=0; i<nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++) {
+            int x = target - nums[i];
+            if (um.find(x) != um.end()) return {um[x], i};
             um[nums[i]] = i;
-        }
-
-        int x;
-        for(int i=0; i<nums.size(); i++) {
-            x = target - nums[i];
-            if (um.find(x) != um.end() and i!= um[x]) return {i, um[x]};
         }
 
         return {0, 0};
