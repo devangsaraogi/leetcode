983f56b90328f0f830a3b4fadfb80a4bd3af39b2
Devang Saraogi <devangsaraogi@outlook.com>
2026-01-30 19:04:09 -0500

    Time: 20 ms (39.69%), Space: 26.7 MB (28.14%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         // hash table
-        unordered_map <string, vector<int>> hmap;
+        unordered_map <string, vector<string>> hmap;
 
         vector<vector<string>> result;
 
@@ -12,17 +12,11 @@ public:
         // sorting each element in copy vector
         for(int i=0; i<strscpy.size(); i++) {
             sort(strscpy[i].begin(), strscpy[i].end());
-            hmap[strscpy[i]].push_back(i);
+            hmap[strscpy[i]].push_back(strs[i]);
         }
 
         // using hash table to get back the final output
-        for (auto k: hmap) {
-            vector<string> dum;
-            for (auto idx: k.second) {
-                dum.push_back(strs[idx]);
-            }
-            result.push_back(dum);
-        }
+        for (auto k: hmap) result.push_back(k.second);
 
         return result;
     }
