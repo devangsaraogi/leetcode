b52501b1024664daec6a009aee17257e6c220ff4
Devang Saraogi <devangsaraogi@outlook.com>
2026-02-04 00:03:39 -0500

    Time: 4 ms (16.87%), Space: 10 MB (54.16%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,60 +1,14 @@
 class Solution {
 public:
-    bool checkPal(string &s, int i){
-        int n=s.size();
-        if(i>=n/2) return true;
-
-        //check
-        if(s[i] != s[n-i-1]) return false;
-        return checkPal(s, i+1);
-    }
-
     bool isPalindrome(string s) {
-        string x = "";
-        for(auto ch: s){
-            if (isalnum(ch)) x+=ch;
-            else continue;
+        if (s.size() == 1) return true;
+
+        for (int i = 0, j = s.size()-1; i<j; i++, j--) {
+            while (i < j && !isalnum(s[i])) i++;
+            while (i < j && !isalnum(s[j])) j--;
+
+            if (tolower(s[i]) != tolower(s[j])) return false;
         }
-        transform(x.begin(), x.end(), x.begin(), ::tolower);
-        return checkPal(x, 0);
+        return true;
     }
 };
-
-// class Solution {
-// public:
-//     bool isPalindrome(string s) {
-//         bool flag = true;
-
-//         transform(s.begin(), s.end(), s.begin(), ::tolower);
-//         // for(auto ch: s){
-//         //     if (int(ch)>=97 && int(ch)<=122) continue;
-//         //     else s.erase(remove(s.begin(), s.end(), ch), s.end());
-//         // }
-
-//         // int head = 0;
-//         // int tail = s.size()-1;
-//         // while (head <= tail) {
-//         //     if (!(s[head] == s[tail])) flag = false;
-//         //     else {
-//         //         head+=1;
-//         //         tail-=1;
-//         //     }
-//         // }
-//         // return flag;
-
-//         int head = 0;
-//         int tail = s.size()-1;
-//         while (head <= tail) {
-//             if (!(isalnum(s[head]))) head+=1;
-//             else if (!(isalnum(s[tail]))) tail-=1;
-
-//             else if (!(s[head] == s[tail])) flag = false;
-//             else {
-//                 head+=1;
-//                 tail-=1;
-//             }
-//         }
-//         return flag;
-        
-//     }
-// };
