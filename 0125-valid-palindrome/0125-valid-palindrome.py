da19c5175a7dd5c49933ec591241ee7a6ded0f77
Devang Saraogi <devangsaraogi@outlook.com>
2026-06-11 21:31:59 -0400

    Time: 16 ms (7.8%), Space: 20.2 MB (29.38%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = "".join(ch for ch in s if ch.isalnum()).lower()
+        print(s)
+
+        p1 = 0
+        p2 = len(s)-1
+
+        while(p1 < len(s) and p2 >= 0):
+            if s[p1] == s[p2]:
+                p1+=1
+                p2-=1
+            else:
+                return False
+
+        return True        
