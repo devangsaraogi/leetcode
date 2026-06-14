94d7975ebb84f21f1b01a974d2ed5e3aa27745a4
Devang Saraogi <devangsaraogi@outlook.com>
2026-06-13 20:29:18 -0400

    Time: 0 ms (100%), Space: 13.4 MB (96.9%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,18 +4,38 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        track = {}
-
-        for i in nums:
-            if track.get(i) == None:
-                track[i] = 0
+        
+        # moore voting algo
+        count=1
+        result=nums[0]
+        for i in nums[1:]:
+            if i == result:
+                count+=1
+            else:
+                if count == 0:
+                    result = i
+                    count = 1
+                else:
+                    count-=1
             
-            track[i] += 1
 
-        sz = len(nums)
-        for k in track.keys():
-            if track[k] > sz//2:
-                return k
+        return result
 
-        return -1
+
+
+
+        # track = {}
+
+        # for i in nums:
+        #     if track.get(i) == None:
+        #         track[i] = 0
+            
+        #     track[i] += 1
+
+        # sz = len(nums)
+        # for k in track.keys():
+        #     if track[k] > sz//2:
+        #         return k
+
+        # return -1
         
