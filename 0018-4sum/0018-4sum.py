aa3b011f8a30dc651257d794614d75fa9f637ef8
Devang Saraogi <devangsaraogi@outlook.com>
2026-06-17 18:44:32 -0400

    Time: 447 ms (22.87%), Space: 19.4 MB (22.33%) - LeetHub

---
+++ 0018-4sum/0018-4sum.py
@@ -0,0 +1,55 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        # nums.sort()
+
+        # store = {}
+
+        # for i in range(len(nums)-2):
+        #     for j in range(i+1, len(nums)-1):
+        #         if nums[i] == nums[j]:
+        #             continue
+                
+        #         sum = nums[i] + nums[j]
+        #         if store.get(sum):
+        #             store[sum].append([nums[i], nums[j]])
+        #         else:
+        #             store[sum] = [[nums[i], nums[j]]]
+
+        # for k in store.keys():
+        #    if -k in store.keys():
+
+        nums.sort()
+
+        result = []
+
+        for i in range(len(nums)-3):
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+
+            for j in range(i+1, len(nums)-2):
+                if j>i+1 and nums[j] == nums[j-1]:
+                    continue
+
+                l = j+1
+                r = len(nums)-1
+
+                while (l < r):
+                    curr_sum = nums[i] + nums[j]
+                    runn_sum = nums[l] + nums[r]
+
+                    if curr_sum > target - runn_sum:
+                        r-=1
+                    elif curr_sum < target - runn_sum:
+                        l+=1
+                    else:
+                        result.append([nums[i], nums[j], nums[l], nums[r]])
+                        l+=1
+                        r-=1
+
+                        while l < r and nums[l] == nums[l - 1]:
+                            l += 1
+                            
+                        while l < r and r < len(nums)-1 and nums[r] == nums[r + 1]:
+                            r -= 1
+
+        return result
